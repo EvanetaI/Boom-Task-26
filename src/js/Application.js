ffdf70ef159785ff34e07a0d95b8065e9cf4c1a4
Evaneta <eftimovaevaneta@gmail.com>
2023-02-03 23:32:55 +0200

    Update Application.js

--- src/js/Application.js
+++ src/js/Application.js
@@ -57,21 +57,22 @@ export default class Application extends EventEmitter {
       response = await fetch(data.next);
       data = await response.json();
       for (let planet of data.results) {
-
-        const box = document.createElement("div");
-          box.classList.add("box");
-          box.innerHTML = this._render({
-            name: planet.name,
-            terrain: planet.terrain,
-            population: planet.population,
-          });
-        document.body.querySelector(".main").appendChild(box);
-        // let HTMLBlock = _create(planet);
-        // document.appendChild(HTMLBlock);
+        this._create(planet);
       }
     }
 
     this._stopLoading();
+  }
+
+  _create(data){
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.innerHTML = this._render({
+      name: data.name,
+      terrain: data.terrain,
+      population: data.population,
+    });
+  document.body.querySelector(".main").appendChild(box);
   }
 
   _startLoading() {
@@ -81,60 +82,3 @@ export default class Application extends EventEmitter {
     this._loading[0].style.display = "none";
   }
 }
-
-// import EventEmitter from "eventemitter3";
-
-// export default class Application extends EventEmitter {
-//   _loading;
-//   static get events() {
-//     return {
-//       READY: "ready",
-//     };
-//   }
-
-//   constructor() {
-//     super();
-//     this.createProgress();
-//     this._load();
-//     this.emit(Application.events.READY);
-//   }
-
-//   createProgress() {
-//     this._loading = document.createElement("progress");
-//     this._loading.innerHTML = `<progress id="file" value="0" max="100"> Loading... </progress>`
-//     document.appendChild(this._loading);
-//   }
-
-//   async _load() {
-//     this._startLoading();
-//     let response = await fetch("https://swapi.boom.dev/api/planets");
-//     let data = await response.json();
-
-//     while (data.next) {
-//       response = await fetch(data.next);
-//       data = await response.json();
-//       for (let planet of data.results) {
-//         let HTMLBlock = _create(planet);
-//         document.appendChild(HTMLBlock);
-//       }
-//     }
-
-//     _stopLoading();
-//   }
-  
-//   _create(HTMLBlock) {
-//     return `
-//     <div>
-//       <h1${HTMLBlock.name}</h1>
-//       <p>Climate: ${HTMLBlock.climate}</p>
-//       <p>Terrain: ${HTMLBlock.terrain}</p>
-//       <p>Population: ${HTMLBlock.population}</p>
-//     </div>`
-//   }
-//   _startLoading() {
-//     this._progress.style.display = "block";
-//   }
-//   _stopLoading() {
-//     this._progress.style.display = "none";
-//   }
-// }
